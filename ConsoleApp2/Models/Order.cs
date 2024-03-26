b0e0c59916627e782da1e68ea9cff39aa258e0e4
Minkin Timur <Minkin_Timur@timelyskills.com>
2024-03-26 17:59:10 +0600

    Add Order

---
+++ ConsoleApp2/Models/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    public class Order
+    {
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        
+        public List<Product> Products { get; set; }
+
+        public Order(string customer, string address)
+        {
+            Customer = customer;
+            Address = address;
+            Products = new List<Product>();
+        }
+
+        public void AddProducts(params Product[] products)
+        {
+            Products.AddRange(products);
+        }
+
+        public override string ToString() 
+        {
+            var result = new StringBuilder();
+            result.Append($"Client Name : {Customer} \n Address : {Address} \n Products : \n");
+            foreach (var product in Products) {
+                result.Append(product.ToString());
+                result.Append("\n");
+            }
+            return result.ToString();   
+        }
+
+
+    }
+}
